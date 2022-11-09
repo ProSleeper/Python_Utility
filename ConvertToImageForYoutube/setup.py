201aa625e17b7d5b990334728d97caf66a0fbeae
kyw <ingngnin@gmail.com>
2022-11-09 18:05:10 +0900

    refactor: 기존의 콘솔창이 나오던 부분을 안나오도록 수정하고 setup.py 파일에 주석을 달아둠.

--- ConvertToImageForYoutube/setup.py
+++ ConvertToImageForYoutube/setup.py
@@ -4,11 +4,11 @@ import sys
 
 
 
-
+# 스프링에 빗대서 설명하자면 의존성 추가라고 보면 된다. 내가 만든 프로그램에서 사용했던 library들을 전부 같이 빌드한다는 옵션이다.
 buildOptions = dict(packages = ["PyQt5","urllib","imghdr","numpy", "cv2", "os", "sys"],  # 1
-excludes = ["scipy.spatial.cKDTree"])
+excludes = ["scipy.spatial.cKDTree"])   # 현재 나는 사용하지 않는 library인데 복붙 하다보니까 들어갔다. 예시로 남겨두자. 여기에 작성하면 해당 library는 빌드에서 제외시킨다.
 
-exe = [Executable("MainWindow.py")]  # 2
+exe = [Executable("MainWindow.py", base="Win32GUI")]  # 2   base="Win32GUI" 를 작성해줘야 프로그램 실행 시 console창이 뜨지 않는다.
 
 # 3
 setup(
