4fe84b0cd9e4e213cc957ddec0589474dce27bb5
kyw <ingngnin@gmail.com>
2022-12-25 00:49:27 +0900

    feat: 유튜브 다운로더 천천히 만드는 중
    현재는 다운은 주소입력하면 가능하고, mp4를 mp3까지도 됐다.
    남은 부분은 128kbps를 320kbps로 바꾸는 작업만 되면 된다.
    (사실 이미 128kbps파일을 320kbps로 바꿔도 아무 의미 없다고 한다. 그냥 내 기분 좋으려고 바꾼다.!)

--- YoutubeDownloader/main.py
+++ YoutubeDownloader/main.py
@@ -1,16 +1,40 @@
+import os
 from asyncio import streams
 from pytube import YouTube
 from pytube import Playlist
+from pydub import AudioSegment
+from moviepy.editor import *
+
+AudioSegment.converter = os.path.join(os.path.dirname(__file__), "ffmpeg")
+
+
 # YouTube('https://youtu.be/Rc0smU6n-vM').streams.first().download()
 
 # 유튜브나 유튜브 뮤직 둘다 playlist를 얻을 수 있는 메서드
-pl = Playlist('https://music.youtube.com/playlist?list=OLAK5uy_nBl9St6mOl7S_8DxyvVOSVcIHm9-5nSHM&feature=shar');
-pl1 = pl[0]
-pl2 = pl[1]
+# pl = Playlist('https://music.youtube.com/playlist?list=OLAK5uy_nBl9St6mOl7S_8DxyvVOSVcIHm9-5nSHM&feature=shar');
+# pl1 = pl[0]
+# pl2 = pl[1]
 
 
-yt = YouTube('https://www.youtube.com/watch?v=-H85_VMl3iI&ab_channel=LikeItMusic')
-# yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-yt.streams.get_audio_only().download()  # get_audio_only를 사용하면 자동적으로 최고음질로 받아준다.
+yt = YouTube('https://music.youtube.com/watch?v=BBM6-svSXjM&feature=share')
+yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+# yt.streams.get_audio_only().download()  # get_audio_only를 사용하면 자동적으로 최고음질로 받아준다.
 
-# yt.streams.filter(bitrate="320kbps", progressive=False).first().download(filename="audio.mp3")
+file_name = yt.streams.first().default_filename
+
+# file_name은 파일이름은 저장되는 mp4파일과 동일한데, 프로그램 상에서는 확장자가 3gpp인가로 표시되어서 뒤에서부터 읽어서 .을 찾아서 .과 확장자를 지우고 .mp4로 저장하는 코드이고, save_name은 mp4를 mp3로 변환해서 저장할 파일 이름이다.
+file_name = file_name[0:file_name.rfind('.')]
+save_name = file_name + ".mp3"
+file_name += ".mp4";
+print(save_name)
+
+# video = VideoFileClip(file_name)
+# video.audio.write_audiofile(save_name)
+
+
+
+# 아무리 뭔짓을 해도 file path를 못 읽는다. 아래 코드는 mp3로 변환된 파일의 bitrate를 320k로 변환하려고 한건데 해당 이유 때문에 안된다.
+# 뭐 다른 방법 사용해보면 되겠지!
+# sound = AudioSegment.from_file(os.path.join(os.path.dirname(__file__), save_name))
+# sound.export("/file", format="mp3", bitrate="320k")
+
