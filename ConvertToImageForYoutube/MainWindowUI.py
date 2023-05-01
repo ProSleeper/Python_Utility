acf8c1334c6856096cea039671b0f84d19130be6
kyw <ingngnin@gmail.com>
2023-05-01 22:49:03 +0900

    refactor: main.py 파일은 실행 코드와 설정 불러오는 부분만 구현 하는 걸로 확정.

--- ConvertToImageForYoutube/MainWindowUI.py
+++ ConvertToImageForYoutube/MainWindowUI.py
@@ -1,10 +1,20 @@
+import sys
 import urllib.parse
 from PyQt5 import QtWidgets, QtCore, QtGui
 from FileConvert import FileConverter
 
+
+class CreateApp():
+    def __init__(self):
+        self.app = QtWidgets.QApplication(sys.argv)
+        MainWindow = MainWidget();
+        sys.exit(self.app.exec_())
+
 # QMainWindow 상속받은 윈도우창이다.
 class MainWidget(QtWidgets.QMainWindow):
+    
     def __init__(self, parent=None):
+        
         super(MainWidget, self).__init__(parent)
 
         # 클래스로 만들어 둔 텍스트에디터와 버튼을 현재의 window에 추가시키기 위한 코드
@@ -16,7 +26,7 @@ class MainWidget(QtWidgets.QMainWindow):
         self.setFixedSize(500, 400)
         self.center();
         self.show()
-
+        
     # 창 가운데 정렬을 위한 메서드
     def center(self):
         qr = self.frameGeometry()   # self(여기서는 QMainWindow)의 GeoMetry() 정보를 불러와 동일한 정보의 가상의 사각형을 생성합니다.
--- ConvertToImageForYoutube/main.py
+++ ConvertToImageForYoutube/main.py
@@ -1,11 +1,8 @@
 # 파일 4개로 나눈 것들의 원본!
-import sys
-from PyQt5 import QtWidgets
-from MainWindowUI import MainWidget
+from MainWindowUI import CreateApp
 
- 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = MainWidget()
-    MainWindow.show()
-    sys.exit(app.exec_())
+    CreateApp();
+    
+    # 추후에 창 크기나, 항상 위에, 창 이동 등 여러가지 설정 사항을 Config 파일로 받는 형태로 변경해보자
+    # 당장은 바꾸거나 수정할 게 거의 없는 것 같다.
