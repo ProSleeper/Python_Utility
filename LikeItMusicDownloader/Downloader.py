2ddaaa7d2cf328e46d4ac8a2a05cee9f2be55d80
kyw <ingngnin@gmail.com>
2024-03-17 00:22:29 +0900

    feat: 현재 pytube 에러로 다운로더 실행 시 다운로드가 안된다.

--- LikeItMusicDownloader/Downloader.py
+++ LikeItMusicDownloader/Downloader.py
@@ -4,15 +4,20 @@ from pytube import YouTube
 
 class LMDDownloader:
     def __init__(self, url:str, save_dir:str):
-        self.url = url;
-        self.save_dir = save_dir;
-        self.yt = YouTube(self.url);
-        self.trim_title = self.__Trim_Title();
+        try:
+            self.url = url
+            self.save_dir = save_dir
+            self.yt = YouTube(self.url)
+            self.trim_title = self.__Trim_Title()
+            
+        except BaseException as e:
+            print(f"[{self.__class__.__name__}], Error: {str(e)}")
+        
         
     def Download(self):
         # download가 완료 되면 다운로드한 file path를 리턴해준다.
         down_path = self.yt.streams.filter(only_audio=True).first().download(output_path = self.save_dir, filename = self.trim_title)
-        return self.yt.title, down_path;
+        return self.yt.title, down_path
         
     def __Trim_Title(self):
-        return f"{self.yt.title.replace(' ', '')}.mp4";
+        return f"{self.yt.title.replace(' ', '')}.mp4"
--- LikeItMusicDownloader/Executor.py
+++ LikeItMusicDownloader/Executor.py
@@ -5,8 +5,8 @@ from Convertor import LMDConvertor
 
 class LMDExecutor:
     def __init__(self, url_list, save_dir:str = ""):
-        self.url_list = url_list;
-        self.save_dir = save_dir;
+        self.url_list = url_list
+        self.save_dir = save_dir
 
     # 여러개의 음악을 한번에 다운받고 싶으면 이 부분을 스레드로 만들면 될듯.
     def DownLoad(self):
@@ -14,11 +14,11 @@ class LMDExecutor:
             for url in self.url_list:
                 if "http" not in url:
                     # 이런 에러나 잘못된 부분에서 log를 남겨놔야 하는데, 일단 넘어가도록 continue로 해두자.
-                    continue;
+                    continue
                 lmddownloader = LMDDownloader(url, self.save_dir)
-                title, down_path = lmddownloader.Download();
-                lmdconvertor = LMDConvertor(title, down_path);
-                lmdconvertor.Convert();
+                title, down_path = lmddownloader.Download()
+                lmdconvertor = LMDConvertor(title, down_path)
+                lmdconvertor.Convert()
         except BaseException as e:
-            print(str(e))
+            print(f"[{self.__class__.__name__}], Error: {str(e)}")
        
